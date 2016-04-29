1d459e7a5fc4c1e3b8cf2a98bb62c7291af73a06
Alyssa <ahudon@uw.edu>
2016-04-29 16:39:33 -0700

    start, with table view cells defined

---
+++ iQuiz/QuizTableCell.swift
@@ -0,0 +1,18 @@
+//
+//  QuizTableCell.swift
+//  iQuiz
+//
+//  Created by iGuest on 4/29/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class QuizTableCell : UITableViewCell {
+   
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var detail: UILabel!
+    
+    @IBOutlet weak var icon: UIImageView!
+}
---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by iGuest on 4/29/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var table: UITableView!
+    
+    var table_data = Array<QuizTableCell>()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
